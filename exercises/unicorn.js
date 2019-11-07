b01021865d34e32add7090f49e81764bf056b53a
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-11-07 16:37:35 -0700

    Add functionality to Unicorn class

--- exercises/unicorn.js
+++ exercises/unicorn.js
@@ -1,7 +1,19 @@
 class Unicorn {
   // Think of a constructor as being similar to initialize in Ruby
   // Instead of setting attributes like @name = name in JavaScript we say this.name = name
-  constructor() {
+  constructor(name, color='white') {
+    this.name = name;
+    this.color = color;
+  }
+
+  isWhite(){
+    if (this.color !== 'white') {
+      return false;
+    }
+  }
+
+  says(input){
+    return `**;* ${input} *;**`
   }
 }
 
