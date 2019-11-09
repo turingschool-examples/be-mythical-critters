397add2be04bbe807583b62796e7583a2182984e
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-11-09 12:34:18 -0700

    Add functionality to Human class

---
+++ exercises/human.js
@@ -0,0 +1,18 @@
+class Human{
+  constructor(name){
+    this.name=name
+    this.encounterCounter=0
+    this.knockedOut=false
+  }
+
+  noticesOgre(){
+    if (this.encounterCounter > 0 &&
+       this.encounterCounter % 3 === 0){
+      return true
+    } else {
+      return false
+    }
+  }
+
+}
+module.exports = Human;
