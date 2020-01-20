4c80757d1ed50c018168d69d4096d314b267032e
Laura Schulz <lrs8810@gmail.com>
2020-01-20 13:30:07 -0700

    Unicorn functionality

--- exercises/unicorn.js
+++ exercises/unicorn.js
@@ -1,7 +1,17 @@
 class Unicorn {
   // Think of a constructor as being similar to initialize in Ruby
   // Instead of setting attributes like @name = name in JavaScript we say this.name = name
-  constructor() {
+  constructor(name, color = 'white') {
+    this.name = name;
+    this.color = color;
+  }
+
+  isWhite() {
+    return this.color === 'white'
+  }
+
+  says(message) {
+    return `**;* ${message} *;**`;
   }
 }
 
