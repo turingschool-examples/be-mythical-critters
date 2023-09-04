40e7d8ff24ac9c98b042049e7f87024ef817b3c3
Andra Helton <andrahelton@gmail.com>
2023-09-04 13:24:00 -0600

    Add passing Unicorn tests

--- exercises/unicorn.js
+++ exercises/unicorn.js
@@ -1,11 +1,16 @@
 class Unicorn {
-  // Think of a constructor as being similar to initialize in Ruby
-  // Instead of setting attributes like @name = name in JavaScript we say this.name = name
-  constructor() {
+  constructor(name, color = 'white') {
+    this.name = name,
+    this.color = color
+  }
+
+  isWhite() {
+    return this.color === 'white'
+  }
+
+  says(message) {
+    return `**;* ${message} *;**`
   }
 }
 
-
-// Notice here we are exporting our class.
-// In order for our test or any other file to be able to know about it we must export it.
 module.exports = Unicorn;
